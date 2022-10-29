6490cb2262204a61437f2a57d3435cef5fa1b509
Faiyas05 <faiyasahamed05@gmail.com>
2022-10-29 10:16:17 +0530

    committing full project into git hub

---
+++ mavanapp/src/main/java/com/Mavenapp/mavanapp/MavanappApplication.java
@@ -0,0 +1,13 @@
+package com.Mavenapp.mavanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavanappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavanappApplication.class, args);
+	}
+
+}
---
+++ mavanapp/src/test/java/com/Mavenapp/mavanapp/MavanappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mavenapp.mavanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavanappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
